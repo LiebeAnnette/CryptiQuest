e5bf0dfe967a78b3cf3dc4ca8bb835d45a4231b7
LiebeAnnette <liebeannette@gmail.com>
2025-05-10 20:03:42 -0500

    beginning to build client side. npm run dev in client terminal pulls up localhost5173

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to CryptiQuest</h1>
+      <p>Discover legendary creatures and haunted places.</p>
+    </div>
+  );
+};
+
+export default Home;
